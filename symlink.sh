5887f97880056d8ca74828906763b49db817b107
Aellas <zacpi@pm.me>
2025-04-19 15:24:06 +0000

    rofi themes & added wlogout

--- symlink.sh
+++ symlink.sh
@@ -32,7 +32,7 @@ for font in "$dotfiles_dir/fonts"/*; do
 done
 
 # Symlink for other directories, excluding themes and fonts
-for dir in hypr waybar kitty scripts rofi; do
+for dir in hypr waybar kitty scripts rofi ml4w wlogout; do
   target="$dotfiles_dir/$dir"
   link="$HOME/.config/$dir"
   create_symlink "$target" "$link"
