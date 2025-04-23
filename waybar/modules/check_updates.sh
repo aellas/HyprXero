3bf21b94295ff700a26fbf0ea2586a46786702ed
DarkXero <81101439+DarkXero-dev@users.noreply.github.com>
2025-04-23 16:09:10 +0300

    Update check_updates.sh

--- waybar/modules/check_updates.sh
+++ waybar/modules/check_updates.sh
@@ -4,9 +4,9 @@ updates=$(checkupdates 2>/dev/null | wc -l)
 
 if [ "$updates" -gt 0 ]; then
   icon=""
+  echo "$icon $updates"
   notify-send "System Updates" "$updates updates are available."
 else
   icon=""
+  echo "$icon"
 fi
-
-echo "$icon $updates"
