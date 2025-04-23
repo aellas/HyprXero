085b49e8b4f461c513dd4a7e56871954e73edf04
Aellas <zacpi@pm.me>
2025-04-23 15:15:26 +0000

    fixed spacers

--- waybar/launch-waybar.sh
+++ waybar/launch-waybar.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
-
-# Path to configs
 WAYBAR_CONFIG_DIR="$HOME/.config/waybar"
 LAPTOP_CONFIG="$WAYBAR_CONFIG_DIR/laptop/config"
 DESKTOP_CONFIG="$WAYBAR_CONFIG_DIR/config"
 STYLE="$WAYBAR_CONFIG_DIR/style.css"
 STYLE2="$WAYBAR_CONFIG_DIR/laptop/style.css"
 
-# Detect battery (i.e., laptop)
-if ls /sys/class/power_supply/ | grep -qi 'BAT'; then
+echo "Checking for battery..."
+ls /sys/class/power_supply/
+
+if ls /sys/class/power_supply/ | grep -qi 'BAT0'; then
     echo "Laptop detected. Starting Waybar with laptop config."
-    waybar -c "$LAPTOP_CONFIG" -s "$STYLE2" &
+    echo "Config: $LAPTOP_CONFIG"
+    echo "Style: $STYLE2"
+    waybar --config "$LAPTOP_CONFIG" --style "$STYLE2"
 else
     echo "Desktop detected. Starting Waybar with desktop config."
-    waybar -c "$DESKTOP_CONFIG" -s "$STYLE" &
+    echo "Config: $DESKTOP_CONFIG"
+    echo "Style: $STYLE"
+    waybar --config "$DESKTOP_CONFIG" --style "$STYLE"
 fi
-
